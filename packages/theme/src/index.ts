9878041d47eba854099d95dc0db051f4ce6d5b4a
Leonard Burton <leonard.burton@futurestandard.co.jp>
2026-04-01 19:47:25 +0900

    Initial theme CSS

---
+++ packages/theme/src/index.ts
@@ -0,0 +1,5 @@
+import "./fonts.css";
+import "./colors.css";
+import "./colors.aliases.css";
+import "./animation.css";
+import "./layout.css";
---
+++ packages/theme/vite.config.ts
@@ -0,0 +1,5 @@
+import { createLibraryConfig } from "@future-standard/build-config";
+
+export default createLibraryConfig({
+  dirname: import.meta.dirname,
+});
